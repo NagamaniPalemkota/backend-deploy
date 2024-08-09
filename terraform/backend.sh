bb1caed4266239121d068c4b217891d9545ea583
NagamaniPalemkota <nagamani.palemkota@gmail.com>
2024-08-09 12:13:31 +0530

    committing resources for jenkins

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -3,4 +3,4 @@
 # environment = $2
 dnf install ansible -y
 pip3.9 install botocore boto3
-ansible-pull -i localhost, -U https://github.com/NagamaniPalemkota/expense-ansible-roles-tf.git main.yaml -e component=$1 -e env=$2
+ansible-pull -i localhost, -U https://github.com/NagamaniPalemkota/expense-ansible-roles-tf.git main.yaml -e component=$1 -e env=$2 -e appVersion=$3
